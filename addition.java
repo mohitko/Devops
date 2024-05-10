185ddd1432f8bba75d0920bacc4c5368b6b8c9a4
mohitko <mohitkodape900@gmail.com>
2024-05-10 23:36:28 +0530

    the codw change in java

---
+++ addition.java
@@ -0,0 +1,21 @@
+//#!/bin/bash
+
+
+
+//#############################
+//# Author : mohit
+//# date : 10 may 24
+//#  addition of the number
+//#############################
+
+
+import java.util.*;
+class addition{
+public static void main(String[] args){
+       int x = 5;
+int y = 6;
+int sum = x + y;
+System.out.println(sum);
+}
+}
+
